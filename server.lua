5fa7557b70141f2927509023a714b4dba7c41c8b
timnboys <timnboys@users.noreply.github.com>
2019-10-06 17:21:53 -0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,19 +1,27 @@
 print("X-Vest Made by Xerxes468893#0001 / Peter Greek")  -- dont remove please
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterCommand("vest", function(source, args, raw)
 	local player = source 
-	if (player > 0) then
+	local xPlayer = ESX.GetPlayerFromId(player)
+	if xPlayer.job.name == 'police' then
 		local version = args[1]
 		TriggerClientEvent("ARPF:vest", source, version)
 		CancelEvent()
+	else
+		TriggerClientEvent('esx:showNotification', _source, "Insufficent Permissions")
 	end
 end, false)
 
 RegisterCommand("removevest", function(source, args, raw)
-	local player = source 
-	if (player > 0) then
+	local player = source
+	local xPlayer = ESX.GetPlayerFromId(player)
+	if xPlayer.job.name == 'police' then
 		TriggerClientEvent("ARPF:vestoff", source)
 		CancelEvent()
+	else
+		TriggerClientEvent('esx:showNotification', _source, "Insufficent Permissions")
 	end
 end, false)
-
